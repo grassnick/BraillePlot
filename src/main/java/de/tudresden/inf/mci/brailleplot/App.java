f3a6bed0a2ab90186c779c622813652ac4f1a624
Georg Graßnick <georg.grassnick@mailbox.tu-dresden.de>
2019-05-21 16:53:19 +0200

    Add project skeleton

---
+++ src/main/java/de/tudresden/inf/mci/brailleplot/App.java
@@ -0,0 +1,11 @@
+package de.tudresden.inf.mci.brailleplot;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
