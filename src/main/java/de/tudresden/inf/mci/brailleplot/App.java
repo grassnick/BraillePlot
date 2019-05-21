bd7f84376ed4af15ac7aba57727f6d858cd777f5
Andrey Ruzhanskiy <andrey.ruzhanskiy@tu-dresden.de>
2019-05-21 18:57:12 +0200

    added Parsing, throws Exceptions dirty

--- src/main/java/de/tudresden/inf/mci/brailleplot/App.java
+++ src/main/java/de/tudresden/inf/mci/brailleplot/App.java
@@ -1,7 +1,8 @@
 package de.tudresden.inf.mci.brailleplot;
 
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Options;
+import org.apache.commons.cli.*;
+
+import java.io.IOException;
 
 
 public class App {
@@ -9,7 +10,9 @@ public class App {
         return "Hello world.";
     }
 
-    public static void parseCLI(){
+    public static void parseCLI(String[] args) throws ParseException {
+
+        // Creating Options object
         Options options = new Options();
         options.addOption("c", "csv", true, "Path to CSV")
                 .addOption("s", "semMap", true, "Literal for semantic mapping")
@@ -18,11 +21,22 @@ public class App {
         String headerForOptions = "Convert csv into braille";
         String footerForOptions = "Report Issues to Leonard Kupper";
         formatter.printHelp("braillegraphics",headerForOptions,options,footerForOptions,true);
+
+        //Create a parser
+        CommandLineParser parser = new DefaultParser();
+
+        //parse the options
+        CommandLine cmd = parser.parse(options, args);
+
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         System.out.println(new App().getGreeting());
-        parseCLI();
+        try {
+            parseCLI(args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 }
