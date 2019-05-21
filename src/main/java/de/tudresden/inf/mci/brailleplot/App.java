cc86ef9357219fb4a42e4916a310d2df25594b46
Andrey Ruzhanskiy <andrey.ruzhanskiy@tu-dresden.de>
2019-05-21 17:19:51 +0200

    added CLI Parser & adjusted build.gradle vor commons apache cli

--- src/main/java/de/tudresden/inf/mci/brailleplot/App.java
+++ src/main/java/de/tudresden/inf/mci/brailleplot/App.java
@@ -1,11 +1,28 @@
 package de.tudresden.inf.mci.brailleplot;
 
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
     }
 
+    public static void parseCLI(){
+        Options options = new Options();
+        options.addOption("c", "csv", true, "Path to CSV")
+                .addOption("s", "semMap", true, "Literal for semantic mapping")
+                .addOption("p", "printer", true, "Printerconfig");
+        HelpFormatter formatter = new HelpFormatter();
+        String headerForOptions = "Convert csv into braille";
+        String footerForOptions = "Report Issues to Leonard Kupper";
+        formatter.printHelp("braillegraphics",headerForOptions,options,footerForOptions,true);
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        parseCLI();
+
     }
 }
