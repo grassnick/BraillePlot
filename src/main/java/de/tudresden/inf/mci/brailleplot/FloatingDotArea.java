4f291aaf23af8d3db89a608fd3ff2a31b673b2b9
LeonardK <s8447445@msx.tu-dresden.de>
2019-05-29 23:43:12 +0200

    Test parameters with FloatingDotArea DocumentBuilder

--- src/main/java/de/tudresden/inf/mci/brailleplot/FloatingDotArea.java
+++ src/main/java/de/tudresden/inf/mci/brailleplot/FloatingDotArea.java
@@ -10,14 +10,68 @@ import java.io.IOException;
 
 public class FloatingDotArea extends AbstractDocumentBuilder {
     private final byte[] mActivateDotArea = new byte[]{0x1B, 0x46};
+    private final byte[] mParameterOrigo = new byte[]{0x4F, 0x52};
+    private final byte[] mParameterWidth = new byte[]{0x57, 0x58};
+    private final byte[] mParameterHeight = new byte[]{0x48, 0x59};
     private byte[] mDotList;
     private final byte[] mSemicolon = new byte[] {0x3B};
     private final byte[] mNewLine = new byte[] {0x0A};
     private final byte mColon = 0x3A;
+    private final byte mComma = 0x2C;
+    private int origoX = -1;
+    private int origoY = -1;
+    private int widthX = -1;
+    private int heightY = -1;
 
-
+    /**
+     * Construct a floating dot area with default origin and size.
+     * @param data
+     * A sequence of bytes containing a newline separated line of colon separated values: xxx.xx:yy.yyy
+     */
     public FloatingDotArea(final byte[] data) {
-            mDocument = this.assemble(data);
+        mDocument = this.assemble(data);
+    }
+
+    /**
+     * Construct a floating dot area with specified origin and default size.
+     * @param data
+     * A sequence of bytes containing a newline separated line of colon separated values: xxx.xx:yy.yyy
+     * @param origoX
+     * Origin x value in centimetres
+     * @param origoY
+     * Origin y value in centimetres
+     */
+    public FloatingDotArea(final byte[] data, final int origoX, final int origoY) {
+        this.origoX = origoX;
+        this.origoY = origoY;
+        mDocument = this.assemble(data);
+    }
+
+    /**
+     * Construct a floating dot area with specified origin and size.
+     * @param data
+     * A sequence of bytes containing a newline separated line of colon separated values: xxx.xx:yy.yyy
+     * @param origoX
+     * Origin x value in centimeters
+     * @param origoY
+     * Origin y value in centimetres
+     * @param widthX
+     * Area width in millimeters
+     * @param heightY
+     * Area height in millimeters
+     */
+    public FloatingDotArea(
+            final byte[] data,
+            final int origoX,
+            final int origoY,
+            final int widthX,
+            final int heightY
+    ) {
+        this.origoX = origoX;
+        this.origoY = origoY;
+        this.widthX = widthX;
+        this.heightY = heightY;
+        mDocument = this.assemble(data);
     }
 
     @Override
@@ -27,18 +81,34 @@ public class FloatingDotArea extends AbstractDocumentBuilder {
         try {
 
             stream.write(mActivateDotArea);
+            if ((origoX >= 0) && (origoY >= 0)) {
+                stream.write(mParameterOrigo);
+                //stream.write("(5.00, 3.00)".getBytes());
+                stream.write("5.00:0.00".getBytes());
+            }
+            if (widthX >= 0) {
+                stream.write(mComma);
+                stream.write(mParameterWidth);
+                stream.write(Integer.toString(widthX).getBytes());
+            }
+            if (heightY >= 0) {
+                stream.write(mComma);
+                stream.write(mParameterHeight);
+                stream.write(Integer.toString(heightY).getBytes());
+            }
             stream.write(mSemicolon);
             stream.write(mNewLine);
 
             stream.write(data);
 
-            stream.write(parseData(data));
+            //stream.write(parseData(data));
 
             stream.write(mSemicolon);
 
         } catch (IOException e) {
             e.getMessage();
         }
+        mDocument = stream.toByteArray();
         return mDocument;
     }
 
