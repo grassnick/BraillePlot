ae814ebf11169cf700c7ba27f43878879bfa01bd
Leonard Kupper <leonard.kupper@mailbox.tu-dresden.de>
2019-07-10 04:19:25 +0200

    Extend bar chart dummy.

--- src/main/java/de/tudresden/inf/mci/brailleplot/rendering/BarChart.java
+++ src/main/java/de/tudresden/inf/mci/brailleplot/rendering/BarChart.java
@@ -1,13 +1,23 @@
 package de.tudresden.inf.mci.brailleplot.rendering;
 
+
 /**
  * A dummy class for BarCharts.
  * @author Leonard Kupper
  * @version 2019.06.28
  */
-public class BarChart extends DiagramStub {
+public class BarChart implements Renderable {
 
-    private double[] dummy = {0.7, 3.1, -1.65, 1.4, -3.8, -2};
+    //private double[] dummy = {0.21, 0.13, -0.3, 0.52, -1.02, 0.65, 0.77, -0.33};
+    private double[] dummy = {500,36,357,-384,473,75,220,356};
+
+    final String getTitle() {
+        return "I am a bar chart.";
+    }
+
+    final double[] getCategories() {
+        return dummy;
+    }
 
     final int getNumberOfCategories() {
         return dummy.length;
@@ -15,6 +25,30 @@ public class BarChart extends DiagramStub {
 
     final double getValue(final int categoryIndex) {
         return dummy[categoryIndex];
+    }
+
+    public double getValueRangeSize() {
+        return (getValueRangeMax() - getValueRangeMin());
+    }
+
+    public double getValueRangeMax() {
+        double max = Double.NEGATIVE_INFINITY;
+        for (double value : dummy) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    public double getValueRangeMin() {
+        double min = Double.POSITIVE_INFINITY;
+        for (double value : dummy) {
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
     }
 
 }
--- src/main/java/de/tudresden/inf/mci/brailleplot/rendering/DiagramStub.java
+++
@@ -1,9 +0,0 @@
-package de.tudresden.inf.mci.brailleplot.rendering;
-
-/**
- * DiagramStub. This and its subclasses are only temporary stubs and have to be replaced with the concrete diagram representation classes.
- * @author Leonard Kupper
- * @version 2019.06.28
- */
-public class DiagramStub implements Renderable {
-}
