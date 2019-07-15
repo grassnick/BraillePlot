66efd50cca6e9b1762609fa107205cb824d08237
Andrey Ruzhanskiy <andrey.ruzhanskiy@tu-dresden.de>
2019-07-15 13:46:38 +0200

    Added more Unittests.

---
+++ src/test/java/de/tudresden/inf/mci/brailleplot/exporter/NormalBuilderTest.java
@@ -0,0 +1,25 @@
+package de.tudresden.inf.mci.brailleplot.exporter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit Tests for the NormalBuilder Class.
+ * @author Andrey Ruzhanskiy
+ * @version 15.07.2019
+ */
+public class NormalBuilderTest {
+
+
+    /**
+     * Test for giving a Null to the NormalBuilder assemble Method.
+     * Expected: NullPointerException.
+     */
+    @Test
+    public void testAssembleWithNull(){
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            NormalBuilder normalF1 = new NormalBuilder();
+            normalF1.assemble(null);
+        });
+    }
+}
--- src/test/java/de/tudresden/inf/mci/brailleplot/exporter/PrintDirectorTest.java
+++ src/test/java/de/tudresden/inf/mci/brailleplot/exporter/PrintDirectorTest.java
@@ -1,35 +1,91 @@
 package de.tudresden.inf.mci.brailleplot.exporter;
 
-import de.tudresden.inf.mci.brailleplot.configparser.Format;
-import de.tudresden.inf.mci.brailleplot.configparser.Printer;
-import de.tudresden.inf.mci.brailleplot.printabledata.MatrixData;
-import de.tudresden.inf.mci.brailleplot.printabledata.SimpleMatrixDataImpl;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
+
+/**
+ * Unit Tests for the PrintDirector Class.
+ * @author Andrey Ruzhanskiy
+ * @version 15.07.2019
+ */
+
 public class PrintDirectorTest {
 
+    // Negative Tests
+
+    /**
+     * Test for giving a Null to the PrintDirector-Constructor.
+     * Expected: NullPointerException.
+     */
     @Test
     public void testNullInConstructor(){
         Assertions.assertThrows(NullPointerException.class, () -> {
-            PrintDirector print = new PrintDirector(null);
+            PrintDirector printF1 = new PrintDirector(null);
         });
     }
 
+    /**
+     * Test for setting a printername (with the static method printerExists) which does not exist in the System.
+     * This should breake if someone registers a printer with the name "kek".
+     * Expected: returns false.
+     */
     @Test
     public void testPrinterDoesNotExist(){
-        Assertions.assertEquals(false, PrintDirector.printerExists("kek"));
+        Assertions.assertFalse(PrintDirector.printerExists("kek"));
     }
-/*
 
+    /**
+     * Test for Constructer of the PrintDirector. Tests if the PrinterConfiguration "NormalPrinter" is accepted.
+     * Expected: Does not throw a Exception.
+     */
     @Test
-    public void testNullPointerInPrintString(){
-        Assertions.assertThrows(NullPointerException.class, () -> {
-            PrintDirector print = new PrintDirector(PrinterConfiguration.NORMALPRINTER);
-            print.print(null, new SimpleMatrixDataImpl<Boolean>(new Printer(), new Format(), 18,
-                    20, true) {
-            });
+    public void testConstructerWithNormalBuilder() {
+        Assertions.assertDoesNotThrow(() -> {
+            PrintDirector printT1 = new PrintDirector(PrinterConfiguration.NORMALPRINTER);
         });
     }
+
+    /**
+     * Test for Constructer of the PrintDirector. Tests if the PrinterConfiguration
+     * "INDEX_EVEREST_D_V4_FLOATINGDOT_PRINTER" is accepted.
+     * Expected: Does not throw a Exception.
+     */
+    @Test
+    public void testConstructerD4WithFloatingDot() {
+        Assertions.assertDoesNotThrow(() -> {
+            PrintDirector printT1 = new PrintDirector(PrinterConfiguration.INDEX_EVEREST_D_V4_FLOATINGDOT_PRINTER);
+        });
+    }
+
+    /**
+     * Test for Constructer of the PrintDirector. Tests if the PrinterConfiguration
+     * "INDEX_EVEREST_D_V4_GRAPHIC_PRINTER" is accepted.
+     * Expected: Does not throw a Exception.
+     */
+    @Test
+    public void testConstructerD4WithGraphicPrint() {
+        Assertions.assertDoesNotThrow(() -> {
+            PrintDirector printT1 = new PrintDirector(PrinterConfiguration.INDEX_EVEREST_D_V4_GRAPHIC_PRINTER);
+        });
+    }
+
+
+
+/*
+    @Test
+    public void testSetWrongPrinterName(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->{
+         PrintDirector printD = new PrintDirector(PrinterConfiguration.NORMALPRINTER);
+         Format format = new Format(new ArrayList<>());
+         Printer printer = new Printer(new ArrayList<>());
+         printD.print("kek", new SimpleMatrixDataImpl<Boolean>(printer, format,12,10,
+                 true));
+        });
+    }
+
 */
+
 }
