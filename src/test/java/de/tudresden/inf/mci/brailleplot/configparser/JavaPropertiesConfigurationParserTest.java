f8e93698dbafefe3a3cacb3b73b12e993cfe3192
Leonard Kupper <leonard.kupper@mailbox.tu-dresden.de>
2019-07-18 16:34:04 +0200

    Restructure tests for new feature functionality and updated configuration files.

--- src/test/java/de/tudresden/inf/mci/brailleplot/configparser/JavaPropertiesConfigurationParserTest.java
+++ src/test/java/de/tudresden/inf/mci/brailleplot/configparser/JavaPropertiesConfigurationParserTest.java
@@ -13,6 +13,8 @@ import java.util.Set;
 
 public class JavaPropertiesConfigurationParserTest {
 
+    public static final String mDefaultConfigPath = getResource("default.properties").getAbsolutePath();
+    public static final String mConcreteConfigPath = getResource("concrete.properties").getAbsolutePath();
     public static Printer mPrinterConfig;
     public static Format mFormatConfig;
 
@@ -25,16 +27,9 @@ public class JavaPropertiesConfigurationParserTest {
     // Correct use testcases
     @Test @BeforeAll
     public static void testSuccessfulParsing() {
-        String defaultConfigPath = getResource("default.properties").getAbsolutePath();
-        String configPath = getResource("concrete.properties").getAbsolutePath();
         Assertions.assertDoesNotThrow(() -> {
-            // Parse defaults
-            ConfigurationParser defaultPropertyParser = new JavaPropertiesConfigurationParser(defaultConfigPath);
-            Printer defaultPrinterConfig = defaultPropertyParser.getPrinter();
-            Format defaultFormatConfig = defaultPropertyParser.getFormat("default");
-
-            // Parse actual properties with defaults set
-            ConfigurationParser parser = new JavaPropertiesConfigurationParser(configPath, defaultPrinterConfig, defaultFormatConfig);
+            // Parse concrete properties and defaults
+            ConfigurationParser parser = new JavaPropertiesConfigurationParser(mConcreteConfigPath, mDefaultConfigPath);
             mPrinterConfig = parser.getPrinter();
             Set<String> properties = mPrinterConfig.getPropertyNames();
             Set<String> formats = parser.getFormatNames();
@@ -44,34 +39,32 @@ public class JavaPropertiesConfigurationParserTest {
     @Test
     public void testCorrectValues() {
         // default values - not overwritten
-        Assertions.assertEquals(2.5, mPrinterConfig.getProperty("max.characterDistance").toDouble());
-        Assertions.assertEquals(false, mPrinterConfig.getProperty("floatingDotSupport").toBool());
-
+        Assertions.assertEquals(200, mPrinterConfig.getProperty("raster.constraint.width").toDouble());
+        Assertions.assertEquals("6-dot", mPrinterConfig.getProperty("raster.type").toString());
         Assertions.assertEquals(10, mFormatConfig.getProperty("margin.bottom").toInt());
 
         // overwritten values
 
+        Assertions.assertEquals(true, mPrinterConfig.getProperty("floatingDot.support").toBool());
         Assertions.assertEquals(0, mFormatConfig.getProperty("margin.left").toInt());
 
         // values without default
-        Assertions.assertEquals("Index Everest-D V4", mPrinterConfig.getProperty("name").toString());
+        Assertions.assertEquals("Dummy Printer", mPrinterConfig.getProperty("name").toString());
+        Assertions.assertEquals(0.05, mPrinterConfig.getProperty("floatingDot.resolution").toDouble());
 
     }
     @Test
     public void testCompatibleTypeConversion() {
-        Assertions.assertEquals("5.0", mPrinterConfig.getProperty("indent.top").toString());
-        Assertions.assertEquals(false, mPrinterConfig.getProperty("max.characterDistance").toBool());
+        Assertions.assertEquals("5.0", mPrinterConfig.getProperty("constraint.top").toString());
+        Assertions.assertEquals(false, mPrinterConfig.getProperty("raster.dotDiameter").toBool());
 
         Assertions.assertEquals(297.0, mFormatConfig.getProperty("page.height").toDouble());
     }
     @Test
     public void testFallbackProperties() {
 
-        String specifiedByConfig[] = {"name", "brailletable", "indent.top", "indent.left",
-                "indent.bottom", "indent.right", "raster.indent.top", "raster.indent.left", "raster.indent.bottom",
-                "raster.indent.right", "raster.dotDistance.horizontal", "raster.dotDistance.vertical",
-                "raster.cellDistance.horizontal", "raster.cellDistance.vertical", "raster.dotDiameter"};
-        String specifiedByFallback[] = {"floatingDotSupport", "max.characterDistance", "raster.dotDiameter"};
+        String specifiedByConfig[] = {"name", "mode", "brailletable", "floatingDot.support", "floatingDot.resolution", "constraint.top", "constraint.left", "raster.dotDistance.horizontal", "raster.dotDistance.vertical", "raster.cellDistance.horizontal", "raster.cellDistance.vertical", "raster.dotDiameter"};
+        String specifiedByFallback[] = {"mode", "brailletable", "floatingDot.support", "constraint.top", "constraint.left", "raster.constraint.top", "raster.constraint.left", "raster.constraint.width", "raster.constraint.height", "raster.type", "raster.dotDistance.horizontal", "raster.dotDistance.vertical", "raster.cellDistance.horizontal", "raster.cellDistance.vertical", "raster.dotDiameter"};
 
         // config shall extend the fallback
         HashSet<String> expectedPropertyNames = new HashSet<>(Arrays.asList(specifiedByConfig));
@@ -87,7 +80,15 @@ public class JavaPropertiesConfigurationParserTest {
     public void testIllegalFile() {
         Assertions.assertThrows(
                 ConfigurationParsingException.class,
-                () -> new JavaPropertiesConfigurationParser("nonexistent.properties")
+                () -> new JavaPropertiesConfigurationParser("nonexistent.properties", mDefaultConfigPath)
+        );
+    }
+    @Test
+    public void testMissingRequired() {
+        String configPath = getResource("missingRequiredPropertyExample.properties").getAbsolutePath();
+        Assertions.assertThrows(
+                IllegalStateException.class,
+                () -> new JavaPropertiesConfigurationParser(configPath, mDefaultConfigPath)
         );
     }
     @Test
@@ -95,7 +96,7 @@ public class JavaPropertiesConfigurationParserTest {
         String configPath = getResource("illegalPropertyNameExample.properties").getAbsolutePath();
         Assertions.assertThrows(
                 ConfigurationValidationException.class,
-                () -> new JavaPropertiesConfigurationParser(configPath)
+                () -> new JavaPropertiesConfigurationParser(configPath, mDefaultConfigPath)
         );
     }
     @Test
@@ -103,20 +104,18 @@ public class JavaPropertiesConfigurationParserTest {
         String configPath = getResource("illegalPropertyValueExample.properties").getAbsolutePath();
         Assertions.assertThrows(
                 ConfigurationValidationException.class,
-                () -> new JavaPropertiesConfigurationParser(configPath)
+                () -> new JavaPropertiesConfigurationParser(configPath, mDefaultConfigPath)
         );
     }
     @Test
     public void testNonexistentFormat() {
-        String configPath = getResource("default.properties").getAbsolutePath();
         Assertions.assertThrows(NoSuchElementException.class, () -> {
-            ConfigurationParser parser = new JavaPropertiesConfigurationParser(configPath);
+            ConfigurationParser parser = new JavaPropertiesConfigurationParser(mConcreteConfigPath, mDefaultConfigPath);
             parser.getFormat("B5");
         });
     }
     @Test
     public void testNonexistentProperties() {
-        String configPath = getResource("default.properties").getAbsolutePath();
         Assertions.assertThrows(NoSuchElementException.class, () -> {
             ValidProperty spooderman = mPrinterConfig.getProperty("spooderman");
         });
@@ -126,11 +125,9 @@ public class JavaPropertiesConfigurationParserTest {
     }
     @Test
     public void testIncompatibleTypeConversion() {
-        Assertions.assertThrows(NumberFormatException.class, () -> mPrinterConfig.getProperty("floatingDotSupport").toInt());
-        Assertions.assertThrows(NumberFormatException.class, () -> mPrinterConfig.getProperty("max.characterDistance").toInt());
+        Assertions.assertThrows(NumberFormatException.class, () -> mPrinterConfig.getProperty("floatingDot.support").toInt());
+        Assertions.assertThrows(NumberFormatException.class, () -> mPrinterConfig.getProperty("raster.cellDistance.horizontal").toInt());
         Assertions.assertThrows(NumberFormatException.class, () -> mPrinterConfig.getProperty("name").toDouble());
-
-        //Assertions.assertThrows(NumberFormatException.class, () -> mFormatConfig.getProperty("isPortrait").toDouble());
     }
 
 }
