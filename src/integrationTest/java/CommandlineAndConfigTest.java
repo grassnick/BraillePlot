ddacdbb340f75b5ae7f499c3afb4721c4de99493
Andrey Ruzhanskiy <andrey.ruzhanskiy@tu-dresden.de>
2019-07-22 13:15:15 +0200

    Added Folderstructure for Integrationtests. Added first stub.

---
+++ src/integrationTest/java/CommandlineAndConfigTest.java
@@ -0,0 +1,11 @@
+import de.tudresden.inf.mci.brailleplot.App;
+import org.junit.jupiter.api.Test;
+
+public class CommandlineAndConfigTest {
+
+    private final App mApp = App.getInstance();
+
+    @Test
+    public void testSomething() {
+    }
+}
