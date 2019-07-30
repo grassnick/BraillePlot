45a9afc4b5ea5f1ef6e67675c0718b80380f3631
Georg Graßnick <georg.grassnick@mailbox.tu-dresden.de>
2019-07-30 15:17:11 +0200

    Update Javadoc

--- src/main/java/de/tudresden/inf/mci/brailleplot/csvparser/package-info.java
+++ src/main/java/de/tudresden/inf/mci/brailleplot/csvparser/package-info.java
@@ -1,5 +1,5 @@
 /**
- * TODO refactor parser ({@link de.tudresden.inf.mci.brailleplot.csvparser.CsvXAlignedParser}, {@link de.tudresden.inf.mci.brailleplot.csvparser.CsvXAlignedCategoriesParser}, {@link de.tudresden.inf.mci.brailleplot.csvparser.CsvDotParser}) classes. Lots of blocks are copied with small modifications.
  * Package for parsing algorithms for CSV files.
+ * TODO refactor parser ({@link de.tudresden.inf.mci.brailleplot.csvparser.CsvXAlignedParser}, {@link de.tudresden.inf.mci.brailleplot.csvparser.CsvXAlignedCategoriesParser}, {@link de.tudresden.inf.mci.brailleplot.csvparser.CsvDotParser}) classes. Lots of blocks are copied with small modifications.
  */
 package de.tudresden.inf.mci.brailleplot.csvparser;
