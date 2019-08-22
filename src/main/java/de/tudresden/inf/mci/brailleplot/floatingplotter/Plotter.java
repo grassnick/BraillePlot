c052bcd280aae5f400812c3241e767c9efe6a01b
Richard Schmidt <richard.schmidt@mailbox.tu-dresden.de>
2019-08-22 15:32:34 +0200

    Refine the structure to conform with the UML diagram for package 'rendering' in the wiki

--- src/main/java/de/tudresden/inf/mci/brailleplot/floatingplotter/Plotter.java
+++ src/main/java/de/tudresden/inf/mci/brailleplot/floatingplotter/Plotter.java
@@ -1,10 +1,11 @@
 package de.tudresden.inf.mci.brailleplot.floatingplotter;
 
+import de.tudresden.inf.mci.brailleplot.layout.PlotCanvas;
 import de.tudresden.inf.mci.brailleplot.rendering.Renderable;
 
 /**
  * Plotter. A functional interface for anything that is able to plot renderable data onto a matrix.
- * This interface also defines a static set of tool methods for basic operations on a plot's data container ({@link de.tudresden.inf.mci.brailleplot.layout.PlotMatrix}).
+ * This interface also defines a static set of tool methods for basic operations on a plot's data container ({@link PlotCanvas}).
  * @param <T> The concrete class implementing {@link Renderable} which can be rasterized with the rasterizer.
  * @author Richard Schmidt
  */
--- src/main/java/de/tudresden/inf/mci/brailleplot/layout/PlotCanvas.java
+++ src/main/java/de/tudresden/inf/mci/brailleplot/layout/PlotCanvas.java
@@ -4,12 +4,12 @@ import de.tudresden.inf.mci.brailleplot.configparser.Format;
 import de.tudresden.inf.mci.brailleplot.configparser.Printer;
 
 /**
- * PlotMatrix. Represents a matrix for plotting on Everest-D V4. Extends AbsractCanvas.
+ * PlotCanvas. Represents a matrix for plotting on Everest-D V4. Extends AbsractCanvas.
  * @author Richard Schmidt
  */
-public class PlotMatrix extends AbstractCanvas {
+public class PlotCanvas extends AbstractCanvas {
 
-    public PlotMatrix(final Printer printer, final Format format) throws InsufficientRenderingAreaException {
+    public PlotCanvas(final Printer printer, final Format format) throws InsufficientRenderingAreaException {
         super(printer, format);
     }
 }
