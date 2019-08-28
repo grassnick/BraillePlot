11a0dd835439c3756e3258b9d654d14c43fbc9af
Richard Schmidt <richard.schmidt@mailbox.tu-dresden.de>
2019-08-28 14:12:41 +0200

    Get started on plotting algorithm

--- src/main/java/de/tudresden/inf/mci/brailleplot/layout/PlotCanvas.java
+++ src/main/java/de/tudresden/inf/mci/brailleplot/layout/PlotCanvas.java
@@ -2,20 +2,30 @@ package de.tudresden.inf.mci.brailleplot.layout;
 
 import de.tudresden.inf.mci.brailleplot.configparser.Format;
 import de.tudresden.inf.mci.brailleplot.configparser.Printer;
+import de.tudresden.inf.mci.brailleplot.printabledata.FloatingPointData;
+import de.tudresden.inf.mci.brailleplot.printabledata.SimpleFloatingPointDataImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Representation of a target onto which an image can be plotted.
  * It wraps a {@link de.tudresden.inf.mci.brailleplot.printabledata.FloatingPointData} instance and describes the plot size and its equidistant layout.
- * @author Richard Schmidt
+ * @author Leonard Kupper and Richard Schmidt
  */
 public class PlotCanvas extends AbstractCanvas {
 
     private final Logger mLogger = LoggerFactory.getLogger(this.getClass());
 
     //floating dot area resolution
-    private float mResolution;
+    private double mResolution;
+
+    // diamensions for Braille characters (e.g. axes, title)
+    private int mCellWidth;
+    private int mCellHeight;
+
+    // cell distances horizontal and vertical
+    private int mCellDistHor;
+    private int mCellDistVer;
 
     /**
      * Constructor. Creates a new PlotCanvas, which is a canvas that represents it pages as instances of
@@ -25,8 +35,42 @@ public class PlotCanvas extends AbstractCanvas {
      * @throws InsufficientRenderingAreaException If the given configuration leads to an printable area of negative
      * size or zero size, e.g. if the sum of defined margins and constraints adds up to be greater than the original page size.
      */
-    public PlotCanvas(final Printer printer, final Format format, final float resolution) throws InsufficientRenderingAreaException {
+    public PlotCanvas(final Printer printer, final Format format, final double resolution) throws InsufficientRenderingAreaException {
         super(printer, format);
         mResolution = resolution;
     }
+
+    public final FloatingPointData<Boolean> getNewPage() {
+        mPageContainer.add(new SimpleFloatingPointDataImpl<Boolean>(mPrinter, mFormat));
+        return getCurrentPage();
+    }
+
+    @SuppressWarnings("unchecked")
+    // This is allowed because the mPageContainer fields are always initialized with the correct type by the page getters,
+    // cannot be accessed from the outside and are never changed anywhere else.
+    public final FloatingPointData<Boolean> getCurrentPage() {
+        if (mPageContainer.size() < 1) {
+            return getNewPage();
+        }
+        return (FloatingPointData<Boolean>) mPageContainer.get(mPageContainer.size() - 1);
+    }
+
+    public void readConfigForResolution() {
+        mLogger.trace("Reading raster specific configuration");
+
+        mResolution = mFormat.getProperty("floatingDot.resolution").toDouble();
+        mCellWidth = mFormat.getProperty("raster.dotDistance.horizontal").toInt() + 2 * mFormat.getProperty("raster.dotDiameter").toInt();
+        mCellHeight = 2 * mFormat.getProperty("raster.dotDistance.vertical").toInt() + 3 * mFormat.getProperty("raster.dotDiameter").toInt();
+        mCellDistHor = mFormat.getProperty("raster.cellDistance.horizontal").toInt();
+        mCellDistVer = mFormat.getProperty("raster.cellDistance.vertical").toInt();
+
+    }
+
+    public final double getResolution() {return mResolution;}
+    public final int getCellWidth() {return mCellWidth;}
+    public final int getCellHeight() {return mCellHeight;}
+    public final int getCellDistHor() {return mCellDistHor;}
+    public final int getCellDistVer() {return mCellDistVer;}
+
+
 }
--- src/main/java/de/tudresden/inf/mci/brailleplot/rendering/floatingplotter/ScatterPlotter.java
+++ src/main/java/de/tudresden/inf/mci/brailleplot/rendering/floatingplotter/ScatterPlotter.java
@@ -3,7 +3,10 @@ package de.tudresden.inf.mci.brailleplot.rendering.floatingplotter;
 import de.tudresden.inf.mci.brailleplot.diagrams.ScatterPlot;
 import de.tudresden.inf.mci.brailleplot.layout.InsufficientRenderingAreaException;
 import de.tudresden.inf.mci.brailleplot.layout.PlotCanvas;
+import de.tudresden.inf.mci.brailleplot.point.Point2DValued;
 import de.tudresden.inf.mci.brailleplot.printabledata.FloatingPointData;
+
+import java.util.Objects;
 
 /**
  * ScatterPlotter. Provides a plotting algorithm for scatter plot data. Extends Plotter.
@@ -15,6 +18,15 @@ final class ScatterPlotter implements Plotter<ScatterPlot> {
     PlotCanvas mCanvas;
     FloatingPointData mData;
 
+    double mResolution;
+    double mPageWidth;
+    double mPageHeight;
+
+    // parameters to be identified by trial
+    double mStepSize = 0.5;
+    int mNumberXTics = 5;
+    int mNumberYTics = 10;
+
     /**
      * Constructor. Create a new plotter for instances of {@link ScatterPlot}.
      */
@@ -23,13 +35,77 @@ final class ScatterPlotter implements Plotter<ScatterPlot> {
 
     /**
      * Plots a {@link ScatterPlot} instance onto a {@link PlotCanvas}.
-     * @param data An instance of {@link ScatterPlot} representing the scatter plot.
+     * @param diagram An instance of {@link ScatterPlot} representing the scatter plot.
      * @param canvas An instance of {@link PlotCanvas} representing the target for the plotter output.
      * @throws InsufficientRenderingAreaException If too little space is available on the {@link PlotCanvas}, this is
      * to display the given diagram.
      */
     @Override
-    public void plot(final ScatterPlot data, final PlotCanvas canvas) throws InsufficientRenderingAreaException {
-        return;
+    public void plot(final ScatterPlot diagram, final PlotCanvas canvas) throws InsufficientRenderingAreaException {
+
+        mDiagram = Objects.requireNonNull(diagram);
+        mCanvas = Objects.requireNonNull(canvas);
+        mData = mCanvas.getCurrentPage();
+        mCanvas.readConfigForResolution();
+        mResolution = mCanvas.getResolution();
+        mPageWidth = mCanvas.getPrintableWidth();
+        mPageHeight = mCanvas.getPrintableHeight();
+
+        // calculating ranges
+        double xRange = mDiagram.getMaxX() - mDiagram.getMinX();
+        double yRange = mDiagram.getMaxY() - mDiagram.getMinY();
+
+        // margins for axes
+        double left_margin = 3 * mCanvas.getCellWidth() + 3 * mCanvas.getCellDistHor();
+        double bottom_margin = mPageHeight - (2 * mCanvas.getCellHeight() + 2 * mCanvas.getCellDistVer());
+
+        // x-axis:
+        double last_value = left_margin;
+        for (double i = left_margin; i <= mPageWidth; i += mStepSize) {
+            mData.addPoint(new Point2DValued<Double, Boolean>(i, bottom_margin, true));
+            last_value = i;
+        }
+
+        // arrows on x-axis
+        mData.addPoint((new Point2DValued<Double, Boolean>(last_value - 0.5, bottom_margin + 0.3, true)));
+        mData.addPoint((new Point2DValued<Double, Boolean>(last_value - 1, bottom_margin + 0.6, true)));
+        mData.addPoint((new Point2DValued<Double, Boolean>(last_value - 0.5, bottom_margin - 0.3, true)));
+        mData.addPoint((new Point2DValued<Double, Boolean>(last_value - 1, bottom_margin - 0.6, true)));
+
+        // tick marks on x-axis
+        double xTickStep = (last_value - left_margin) / mNumberXTics;
+        for (int i = 1; i < mNumberXTics + 1; i++) {
+            mData.addPoint((new Point2DValued<Double, Boolean>(left_margin + i * xTickStep, bottom_margin + 0.5, true)));
+            mData.addPoint((new Point2DValued<Double, Boolean>(left_margin + i * xTickStep, bottom_margin + 1, true)));
+            mData.addPoint((new Point2DValued<Double, Boolean>(left_margin + i * xTickStep, bottom_margin + 1.5, true)));
+            mData.addPoint((new Point2DValued<Double, Boolean>(left_margin + i * xTickStep, bottom_margin - 0.5, true)));
+            mData.addPoint((new Point2DValued<Double, Boolean>(left_margin + i * xTickStep, bottom_margin - 1, true)));
+            mData.addPoint((new Point2DValued<Double, Boolean>(left_margin + i * xTickStep, bottom_margin - 1.5, true)));
+        }
+
+        // y-axis:
+        last_value = bottom_margin;
+        for (double i = bottom_margin; i >= 0; i -= mStepSize) {
+            mData.addPoint(new Point2DValued<Double, Boolean>(left_margin, i, true));
+            last_value = i;
+        }
+
+        // arrows on y-axis
+        mData.addPoint((new Point2DValued<Double, Boolean>(left_margin - 0.5, last_value + 0.3, true)));
+        mData.addPoint((new Point2DValued<Double, Boolean>(left_margin - 1, last_value + 0.6, true)));
+        mData.addPoint((new Point2DValued<Double, Boolean>(left_margin + 0.5, last_value + 0.3, true)));
+        mData.addPoint((new Point2DValued<Double, Boolean>(left_margin + 1, last_value + 0.6, true)));
+
+        // tick marks on y-axis
+        double yTickStep = (bottom_margin - last_value) / mNumberYTics;
+        for (int i = 1; i < mNumberYTics + 1; i++) {
+            mData.addPoint((new Point2DValued<Double, Boolean>(left_margin + 0.5, bottom_margin + i * yTickStep, true)));
+            mData.addPoint((new Point2DValued<Double, Boolean>(left_margin + 1, bottom_margin + i * yTickStep, true)));
+            mData.addPoint((new Point2DValued<Double, Boolean>(left_margin + 1.5, bottom_margin + i * yTickStep, true)));
+            mData.addPoint((new Point2DValued<Double, Boolean>(left_margin - 0.5, bottom_margin + i * yTickStep, true)));
+            mData.addPoint((new Point2DValued<Double, Boolean>(left_margin - 1, bottom_margin + i * yTickStep, true)));
+            mData.addPoint((new Point2DValued<Double, Boolean>(left_margin - 1.5, bottom_margin + i * yTickStep, true)));
+        }
+
     }
 }
