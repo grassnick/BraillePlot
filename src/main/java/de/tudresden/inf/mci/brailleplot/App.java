07f7384ab316812a7461164ac95915f3ae00af55
Leonard Kupper <leonard.kupper@mailbox.tu-dresden.de>
2019-09-12 09:54:18 +0200

    Remove unused import.

--- src/main/java/de/tudresden/inf/mci/brailleplot/App.java
+++ src/main/java/de/tudresden/inf/mci/brailleplot/App.java
@@ -42,7 +42,6 @@ import tec.units.ri.unit.MetricPrefix;
 import javax.measure.Quantity;
 import javax.measure.quantity.Length;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
