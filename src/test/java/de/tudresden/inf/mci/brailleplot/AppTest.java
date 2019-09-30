929ed65dd3f7e67cbf3c27fd8105de13bcfbbc44
Andrey Ruzhanskiy <andrey.ruzhanskiy@tu-dresden.de>
2019-09-30 18:00:08 +0200

    Added more test

--- src/test/java/de/tudresden/inf/mci/brailleplot/AppTest.java
+++ src/test/java/de/tudresden/inf/mci/brailleplot/AppTest.java
@@ -1,6 +1,8 @@
 package de.tudresden.inf.mci.brailleplot;
 
 import de.tudresden.inf.mci.brailleplot.App;
+import de.tudresden.inf.mci.brailleplot.configparser.ConfigurationParsingException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,5 +17,9 @@ public class AppTest {
         assertEquals(mApp, App.getInstance());
     }
 
+    @Test
+    public void smokeTest() {
+        Assertions.assertEquals(0, mApp.run(new String[]{"-p", "src/main/resources/config/index_everest_d_v4.properties"}));
+    }
     // TODO Add system tests
 }
--- src/test/java/de/tudresden/inf/mci/brailleplot/rendering/BrailleTextTest.java
+++ src/test/java/de/tudresden/inf/mci/brailleplot/rendering/BrailleTextTest.java
@@ -1,11 +1,17 @@
 package de.tudresden.inf.mci.brailleplot.rendering;
 
+import de.tudresden.inf.mci.brailleplot.layout.Rectangle;
+import de.tudresden.inf.mci.brailleplot.rendering.language.BrailleLanguage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class BrailleTextTest {
 
     @Test
-    public void testConstructor() {
-        BrailleText text = new
+    public  void setUp() {
+        Assertions.assertDoesNotThrow(() -> {
+            BrailleText text = new BrailleText("test", new Rectangle(0, 0, 0, 0), BrailleLanguage.Language.DE_BASISSCHRIFT);
+        });
     }
 }
