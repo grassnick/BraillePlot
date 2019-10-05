6952c2c6a7094fda206471b0ea2e4cf3819a26f7
Georg Graßnick <georg.grassnick@mailbox.tu-dresden.de>
2019-10-05 16:29:13 +0200

    Remove duplicate method
    Is already implemented in specialized inheriting class.

--- src/main/java/de/tudresden/inf/mci/brailleplot/diagrams/BarChart.java
+++ src/main/java/de/tudresden/inf/mci/brailleplot/diagrams/BarChart.java
@@ -2,9 +2,6 @@ package de.tudresden.inf.mci.brailleplot.diagrams;
 
 import de.tudresden.inf.mci.brailleplot.datacontainers.PointList;
 import de.tudresden.inf.mci.brailleplot.datacontainers.PointListContainer;
-import de.tudresden.inf.mci.brailleplot.point.Point2DDouble;
-
-import java.util.Iterator;
 
 /**
  * Representation of a bar chart with basic data functions. Extends {@link Diagram}.
@@ -15,29 +12,5 @@ public class BarChart extends Diagram {
 
     public BarChart(final PointListContainer<PointList> data) {
         super(data);
-    }
-
-    /**
-     * Adds all y-values of one data series.
-     * @return double maximum y-value
-     */
-    public double getCumulatedMaxY() {
-        double maxY = 0;
-
-        for (PointList list : mData) {
-            Iterator<Point2DDouble> smallIt = list.getListIterator();
-            double current = 0;
-            while (smallIt.hasNext()) {
-                Point2DDouble point = smallIt.next();
-                if (point.getY() >= 0) {
-                    current += point.getY();
-                }
-            }
-
-            if (current > maxY) {
-                maxY = current;
-            }
-        }
-        return maxY;
     }
 }
